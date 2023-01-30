3fed29a8f8d60baa7f2c463a147128f346a2dcc9
Andrey.Lutskiy <luzkiyac@mail.ru>
2023-01-30 12:30:16 +0300

    Учебный материал

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person sarah = new Person("Sarah", 30);
+        System.out.println("sarah.name = " + sarah.getName());
+        System.out.println("sarah.age = " + sarah.getAge());
+        sarah.setAge(31);
+        System.out.println("sarah.getAge() = " + sarah.getAge());
+        Person jon = new Person("Jon", 13);
+        System.out.println("jon.name = " + jon.getName());
+        System.out.println("jon.age = " + jon.getAge());
     }
+
 }
